a1e63a6621e45953e4d7c0576e83d89300a2984c
kevin-maciel <kevin.maciel@moxit.net>
2022-10-05 11:19:01 -0300

    add entidades materia prima y cajas

---
+++ ProjectDemoXaf/ProjectDemoXaf.Module/BusinessObjects/Cajas.cs
@@ -0,0 +1,37 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace ProjectDemoXaf.Module.BusinessObjects
+{
+    [NavigationItem("Cajas del CD")]
+    public class Cajas : BaseObject
+    {
+        public Cajas(Session session) : base(session) { }
+        int id;
+        public int Id
+        {
+            get { return id; }
+            set { SetPropertyValue(nameof(Id), ref id, value); }
+        }
+        string caja;
+        public string Caja
+        {
+            get { return caja; }
+            set { SetPropertyValue(nameof(Caja), ref caja, value); }
+        }
+        string descripcion;
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { SetPropertyValue(nameof(Descripcion), ref descripcion, value); }
+        }
+        DateTime fecha;
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { SetPropertyValue(nameof(Fecha), ref fecha, value); }
+        }
+
+    }
+}
